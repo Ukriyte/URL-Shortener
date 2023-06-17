2e9c00430cdae8b8dae7489f1348c41780f6cd6e
Anurag Chugh <nrgchugh@gmail.com>
2023-06-18 01:25:22 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -30,40 +30,33 @@ app.post('/shortUrls', async (req,res)=>{
 
 //Sending search request to backend from searchInput field and finding matching
 //value in database and rendering new site with link that was searched for
-app.post('/search1', async (req,res) =>{
-    const temp = await shortUrl.findOne({Notes : req.body.searchInput1}) 
-    if(temp==null){
-        return res.sendStatus(404)
+app.post('/search', async (req,res) =>{
+    const temp1 = await shortUrl.findOne({Notes : req.body.searchInput}) 
+    if(temp1==null){
+        const temp2 = await shortUrl.findOne({short : req.body.searchInput}) 
+        if(temp2==null){
+            const temp3 = await shortUrl.findOne({full : req.body.searchInput})
+            if(temp3==null){
+                return res.sendStatus(404)
+            }
+            else{
+                res.render('search', {longsearch1: temp3.full, longsearch2: temp3.short,
+                    longsearch3: temp3.Clicks, longsearch4: temp3.Notes });
+                } 
+        }
+        else{
+            res.render('search', {longsearch1: temp2.full, longsearch2: temp2.short,
+                longsearch3: temp2.Clicks, longsearch4: temp2.Notes });
+            }
     }
     else{
-    res.render('search', {longsearch1: temp.full, longsearch2: temp.short,
-        longsearch3: temp.Clicks, longsearch4: temp.Notes });
-    }
+        res.render('search', {longsearch1: temp1.full, longsearch2: temp1.short,
+            longsearch3: temp1.Clicks, longsearch4: temp1.Notes });
+        }
+    
 })
+//1 route for three different parameters
 
-app.post('/search2', async (req,res) =>{
-    const temp = await shortUrl.findOne({short : req.body.searchInput2}) 
-    if(temp==null){
-        return res.sendStatus(404)
-    }
-    else{
-    res.render('search', {longsearch1: temp.full, longsearch2: temp.short,
-        longsearch3: temp.Clicks, longsearch4: temp.Notes });
-    }
-})
-
-app.post('/search3', async (req,res) =>{
-    const temp = await shortUrl.findOne({full : req.body.searchInput3}) 
-    if(temp==null){
-        return res.sendStatus(404)
-    }
-    else{
-    res.render('search', {longsearch1: temp.full, longsearch2: temp.short,
-        longsearch3: temp.Clicks, longsearch4: temp.Notes });
-    }
-})
-
-//3 routes for 3 search boxes
 
 //When the shorturl is searched for, this will be invoked and if the shortUrl exists
 //then clicks will be incremented by one and saved to database
